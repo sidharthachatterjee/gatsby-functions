7f4950574d2272f2cef201030921e0c2b21876c3
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-11-25 21:17:29 +0530

    Add api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [time, setTime] = useState(``)
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const { data } = await fetch(`/functions/time.js`).then(res => res.json())
+      setTime(new Date(data.time)).toLocaleTimeString("en-US")
+    }, 5000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  return (
+    <div>
+      <h1>The time right now is {time}</h1>
+    </div>
+  )
 }
